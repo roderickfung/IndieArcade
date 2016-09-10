3f3dad6eeeb9073982912f9d2f7b9f3335d9c793
mavyrick <jrsorokin@gmail.com>
2016-09-10 14:56:29 -0700

    Added Validations

--- app/models/arcade.rb
+++ app/models/arcade.rb
@@ -1,12 +1,13 @@
 class Arcade < ApplicationRecord
 
-  validates :title, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 25 }
-  validates :address, presence: true
-  validates :phone_number, presence: true
-  validates :website, presence: true
   has_many :arcade_games, dependent: :destroy
   has_many :games, through: :arcade_game
   geocoded_by :address   # can also be an IP address
   after_validation :geocode
+
+  validates :title, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 25 }
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :website, presence: true
 
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,16 +2,8 @@ class Game < ApplicationRecord
   has_many :reviews
   belongs_to :user
 
-<<<<<<< HEAD
   validates :title, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 25 }
   validates :purchase_url, presence: false, uniqueness: {case_sensitive: false}
-  validates :game_description, presence: false, length: { maximum: 500 }
+  validates :game_description, presence: true, length: { maximum: 500 }
   validates :image, presence: true
-=======
-  has_many :tag_games, dependent: :destroy
-  has_many :tags, through: :tag_game
-
-  has_many :arcade_games, dependent: :destroy
-  has_many :arcades, through: :arcade_game
->>>>>>> 1ea2528a499c881f1ab4cbfcd9e3a5458174696b
 end
