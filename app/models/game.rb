e919b1caf0b6344700aa254bbbbceab2ea066e87
Bryce Hopkins <brycehopkins@gmail.com>
2016-09-10 20:20:27 -0700

    Fix Tag association and add show partials

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,20 +1,19 @@
 class Game < ApplicationRecord
+    has_many :tag_games, dependent: :destroy
+    has_many :tags, through: :tag_games
 
-  has_many :tag_games, dependent: :destroy
-  has_many :tags, through: :tag_game
+    has_many :arcades_games, dependent: :destroy
+    has_many :arcades, through: :arcade_games
 
-  has_many :arcade_games, dependent: :destroy
-  has_many :arcades, through: :arcade_game
+    has_many :reviews
+    belongs_to :user
 
-  has_many :reviews
-  belongs_to :user
+    validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 25 }
+    validates :purchase_url, presence: false, uniqueness: { case_sensitive: false }
+    validates :game_description, presence: true, length: { maximum: 500 }
+    validates :image, presence: true
 
-  validates :title, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 25 }
-  validates :purchase_url, presence: false, uniqueness: {case_sensitive: false}
-  validates :game_description, presence: true, length: { maximum: 500 }
-  validates :image, presence: true
-
-  def self.search(search)
-    where("title ILIKE ? OR game_description ILIKE ?", "%#{search}%", "%#{search}%")
-  end
+    def self.search(search)
+        where('title ILIKE ? OR game_description ILIKE ?', "%#{search}%", "%#{search}%")
+    end
 end
