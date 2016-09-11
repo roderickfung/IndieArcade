2393466dc0c2acf0f9f1a119a09f6a475bab6b5a
Hyun Ahn <rationality6@gmail.com>
2016-09-11 16:17:42 -0700

    Add mathod.

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,17 @@ class Review < ApplicationRecord
   validates :playability, presence: true
   validates :difficulty, presence: true
 
+  def graph_parse(value)
+    if value == 5
+      return 'width: 100%'
+    elsif value == 4
+      return 'width: 80%'
+    elsif value == 3
+      return 'width: 60%'
+    elsif value == 2
+      return 'width: 40%'
+    elsif value == 1
+      return 'width: 20%'
+    end
+  end
 end
