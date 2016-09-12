615be2d3ffb98cb93c6883ac56632338078f085a
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-09-11 19:49:36 -0700

    rebasing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911223322) do
+ActiveRecord::Schema.define(version: 20160911210808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160911223322) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.date     "date"
-    t.string   "game"
     t.index ["user_id"], name: "index_games_on_user_id", using: :btree
   end
 
