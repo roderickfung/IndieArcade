6dc587e9eb4c277bb4e21b6d3c06f34d26e6904e
Bryce Hopkins <brycehopkins@gmail.com>
2016-09-13 16:30:54 -0700

    first heroku deployment

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -4,6 +4,7 @@ var planeSize = 1245000;
 var totalObjects = 50000;
 var frame = 60;
 
+
 var container = document.createElement('div');
 document.body.appendChild(container);
 
@@ -16,18 +17,18 @@ var scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0x23233f, 1, 300000);
 
 var uniforms = {
-  time: {
-    type: "f",
-    value: 0.0
-  }
+    time: {
+        type: "f",
+        value: 0.0
+    }
 };
 
 var material = new THREE.ShaderMaterial({
-  uniforms: uniforms,
-  vertexShader: document.getElementById('vertexShader').textContent,
-  fragmentShader: document.getElementById('fragmentShader').textContent,
-  wireframe: true,
-  fog:true
+    uniforms: uniforms,
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    wireframe: true,
+    fog: true
 });
 
 var plane = new THREE.Mesh(new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition), material);
@@ -38,15 +39,15 @@ scene.add(plane);
 var geometry = new THREE.Geometry();
 
 for (i = 0; i < totalObjects; i++) {
-  var vertex = new THREE.Vector3();
-  vertex.x = Math.random() * planeSize - (planeSize * .5);
-  vertex.y = (Math.random() * 100000) + 10000;
-  vertex.z = Math.random() * planeSize - (planeSize * .5);
-  geometry.vertices.push(vertex);
+    var vertex = new THREE.Vector3();
+    vertex.x = Math.random() * planeSize - (planeSize * .5);
+    vertex.y = (Math.random() * 100000) + 10000;
+    vertex.z = Math.random() * planeSize - (planeSize * .5);
+    geometry.vertices.push(vertex);
 }
 
 var material = new THREE.ParticleBasicMaterial({
-  size: 400
+    size: 400
 });
 var particles = new THREE.ParticleSystem(geometry, material);
 
@@ -59,10 +60,10 @@ container.appendChild(renderer.domElement);
 render();
 
 function render() {
-  requestAnimationFrame(render);
-  camera.position.z -= 150;
-  uniforms.time.value = frame;
-  frame += .03;
-  //  dateVerts();
-  renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    camera.position.z -= 150;
+    uniforms.time.value = frame;
+    frame += .03;
+    //  dateVerts();
+    renderer.render(scene, camera);
 }
