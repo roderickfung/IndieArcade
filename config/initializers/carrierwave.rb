2c8d21ab93a6d70e7339355da94d63bf04742fe9
Bryce Hopkins <brycehopkins@gmail.com>
2016-09-14 01:34:55 -0700

    removed invalid config option

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,6 @@ CarrierWave.configure do |config|
   }
 
   config.aws_credentials = {
-    provider:              'AWS',
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:            'us-west-2' # Required
