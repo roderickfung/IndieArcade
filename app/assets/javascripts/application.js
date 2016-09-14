3bd64a0a15df60130fe4bb3843bcfc3c6953536b
Bryce Hopkins <brycehopkins@gmail.com>
2016-09-13 17:18:50 -0700

    gmaps fix 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require chosen-jquery
@@ -18,24 +19,23 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require underscore
-//= require gmaps/google
-$(document).ready(function(){
-  $(document).on('turbolinks:load', function(){
-    $(function() {
-      //----- OPEN
-      $('[data-popup-open]').on('click', function(e)  {
-          var targeted_popup_class = jQuery(this).attr('data-popup-open');
-          $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+$(document).ready(function() {
+    $(document).on('turbolinks:load', function() {
+        $(function() {
+            //----- OPEN
+            $('[data-popup-open]').on('click', function(e) {
+                var targeted_popup_class = jQuery(this).attr('data-popup-open');
+                $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
 
-          e.preventDefault();
-      });
+                e.preventDefault();
+            });
 
-      //----- CLOSE
-      $('[data-popup-close]').on('click', function(e)  {
-          var targeted_popup_class = jQuery(this).attr('data-popup-close');
-          $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
-          e.preventDefault();
-      });
+            //----- CLOSE
+            $('[data-popup-close]').on('click', function(e) {
+                var targeted_popup_class = jQuery(this).attr('data-popup-close');
+                $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+                e.preventDefault();
+            });
+        });
     });
-  });
 });
